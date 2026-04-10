812e5f52844fd74d8e082d5cc0d7738a6936f4e2
log114 <1142159325@qq.com>
2026-04-10 09:51:28 +0800

    0.13.1

--- yikupayloadSDK/src/main/java/com/yiku/yikupayloadSDK/protocol/EmitterProtocol.kt
+++ yikupayloadSDK/src/main/java/com/yiku/yikupayloadSDK/protocol/EmitterProtocol.kt
@@ -1,5 +1,5 @@
 package com.yiku.yikupayloadSDK.protocol;
 
 
-const val EMITTER_LUNCH = 0x11
-const val EMITTER_STATUS = 0x10
+const val EMITTER_LUNCH = 0x11 // 新版里面，自动反馈，但是需要开启安全开关才有效
+const val EMITTER_STATUS = 0x10 // 新版里面已经不需要这命令
